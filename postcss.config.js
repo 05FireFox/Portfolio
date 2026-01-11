1d92c310c74012cfe6f2ffce629868ef0b9ca971
Vankara Jishnu Kali Praneet <vankarajishnukalipraneet05@gmail.com>
2026-01-12 00:21:22 +0530

    Update 3

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
->>>>>>> 4f0939b2feefb43c4730a026ba1545fad69be384
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -18,15 +17,4 @@ module.exports = {
     },
   },
   plugins: [],
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
->>>>>>> 4f0939b2feefb43c4730a026ba1545fad69be384
 }
